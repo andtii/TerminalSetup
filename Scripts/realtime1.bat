f64eb8cca49e9d1501134cfab869bfbacb3ebf8c
Andreas Ekdahl <andy@ekdahls.net>
2022-09-20 12:32:41 +0200

    Set urls for realtime

--- Scripts/realtime1.bat
+++ Scripts/realtime1.bat
@@ -1,3 +1,3 @@
 cd "C:\Dev\OmniaCloud\dev\src\Omnia.Hub.Realtime.Web\bin\Debug\net7.0"
 set ASPNETCORE_ENVIRONMENT=Development
-dotnet Omnia.Hub.Realtime.Web.dll 11112 30001
+dotnet Omnia.Hub.Realtime.Web.dll 11112 30001 --urls https://127.0.0.1:5001
--- Scripts/realtime2.bat
+++ Scripts/realtime2.bat
@@ -1,3 +1,3 @@
 cd "C:\Dev\OmniaCloud\dev\src\Omnia.Hub.Realtime.Web\bin\Debug\net7.0"
 set ASPNETCORE_ENVIRONMENT=Development
-dotnet Omnia.Hub.Realtime.Web.dll 11113 30002
+dotnet Omnia.Hub.Realtime.Web.dll 11113 30002 --urls https://127.0.0.1:5002
